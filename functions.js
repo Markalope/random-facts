5f436434e31199e218532431242563a2a1cb8f0f
Mark Myers <markalope@gmail.com>
2018-04-15 12:20:29 -0400

    added comments to functions file and also CSS to the index.html

---
+++ functions.js
@@ -0,0 +1,13 @@
+/*
+This contains functions for selecting random array items.
+The array items construct a randomized sentence.
+The maximum random number is the length of each array.
+/*
+
+//function for generating random beginning of sentence
+
+//function for selecting random fact
+
+//array of sentence beginings
+
+//array of random facts
